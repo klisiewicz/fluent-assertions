256c6bf6abe4d559bbf0c5b5cfdee49ef6c8eeda
klisiewicz <karol.lisiewicz@gmail.com>
2020-07-08 12:39:19 +0200

    Refactor: Greater and less assertions.

--- lib/src/numerical_assertions.dart
+++ lib/src/numerical_assertions.dart
@@ -3,25 +3,26 @@ import 'package:test/test.dart';
 extension NumericalAssertions on num {
   /// Asserts that the value is greater or equal to [expected].
   void shouldBeGreaterOrEqualTo(num expected) =>
-      expect(this >= expected, isTrue);
+      expect(this, greaterThanOrEqualTo(expected));
 
   /// Asserts that the value is not greater or equal to [expected].
   void shouldNotBeGreaterOrEqualTo(num expected) => shouldBeLessThan(expected);
 
   /// Asserts that the value is greater than [expected].
-  void shouldBeGreaterThan(num expected) => expect(this > expected, isTrue);
+  void shouldBeGreaterThan(num expected) => expect(this, greaterThan(expected));
 
   /// Asserts that the value is not greater than [expected].
   void shouldNotBeGreaterThan(num expected) => shouldBeLessOrEqualTo(expected);
 
   /// Asserts that the value is less or equal to [expected].
-  void shouldBeLessOrEqualTo(num expected) => expect(this <= expected, isTrue);
+  void shouldBeLessOrEqualTo(num expected) =>
+      expect(this, lessThanOrEqualTo(expected));
 
   /// Asserts that the value is not less or equal to [expected].
   void shouldNotBeLessOrEqualTo(num expected) => shouldBeGreaterThan(expected);
 
   /// Asserts that the value is less than [expected].
-  void shouldBeLessThan(num expected) => expect(this < expected, isTrue);
+  void shouldBeLessThan(num expected) => expect(this, lessThan(expected));
 
   /// Asserts that the value is not less than [expected].
   void shouldNotBeLessThan(num expected) => shouldBeGreaterOrEqualTo(expected);
