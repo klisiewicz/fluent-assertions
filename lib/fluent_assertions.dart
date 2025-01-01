e7b025bd3c21e3af2f6d0ef9d097b0da71318c9c
klisiewicz <klisiewicz@users.noreply.github.com>
2025-01-01 10:59:07 +0100

    0.5.0 renamed base_assertions.dart to basic_assertions.dart

--- lib/fluent_assertions.dart
+++ lib/fluent_assertions.dart
@@ -3,7 +3,7 @@
 /// It uses Dart's Extension Functions to provide a fluent wrapper around test assertions.
 library fluent_assertions;
 
-export 'src/base/base_assertions.dart';
+export 'src/basic/basic_assertions.dart';
 export 'src/iterable/iterable_assertions.dart';
 export 'src/numerical/numerical_assertions.dart';
 export 'src/string/string_assertions.dart';
